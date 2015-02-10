cd2fc924ce37e388bb9834496a126a0d407d3b55
fisherk <fisherk@192.168.1.73>
2015-02-10 16:41:46 -0700

    added parser

---
+++ src/com/keithfisher/superecho/CalanderEntry.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.keithfisher.superecho;
+   
+    
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+
+/**
+ *
+ * @author fisherk
+ */
+public class CalanderEntry {
+    
+ 
+
+// ...
+
+// Initialize Calendar service with valid OAuth credentials
+Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
+    .setApplicationName("applicationName").build();
+
+// Quick-add an event
+String eventText = "Appointment at Somewhere on June 3rd 10am-10:25am";
+Event createdEvent =
+    service.events().quickAdd('primary').setText(eventText).execute();
+
+System.out.println(createdEvent.getId());
+    
+}

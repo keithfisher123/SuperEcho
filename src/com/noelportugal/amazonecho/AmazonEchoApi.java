f64eec28bcfd4c80213c119fc876c524784ad87d
fisherk <fisherk@localhost>
2015-02-15 12:25:54 -0700

    Added parser

--- src/com/noelportugal/amazonecho/AmazonEchoApi.java
+++ src/com/noelportugal/amazonecho/AmazonEchoApi.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Logger;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
@@ -40,63 +41,67 @@ import org.jsoup.select.Elements;
  * @author nportuga
  */
 public class AmazonEchoApi {
-    
+
     private final String BASE_URL;
     private final String USERNAME;
     private final String PASSWORd;
     private final HttpClient httpclient = HttpClientBuilder.create().build();
+    private String LastText = "";
+    //static final Logger log= new Logger();
+    
 
-    public AmazonEchoApi(String base_url, String username, String password){
+    public AmazonEchoApi(String base_url, String username, String password) {
+        
         this.BASE_URL = base_url;
         this.USERNAME = username;
         this.PASSWORd = password;
     }
-    
-    public String httpGet(String url){
+
+    public String httpGet(String url) {
         String output = "";
         try {
-                        
+
             HttpGet httpGet = new HttpGet(BASE_URL + url);
             httpGet.setHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13");
             HttpResponse httpResponse = httpclient.execute(httpGet);
             StatusLine responseStatus = httpResponse.getStatusLine();
-        //  System.out.println(responseStatus);
+            //  System.out.println(responseStatus);
             int statusCode = responseStatus.getStatusCode();
             if (statusCode == 200) {
                 httpResponse.getEntity();
                 output = new BasicResponseHandler().handleResponse(httpResponse);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err.println("httpGet Error: " + e.getMessage());
         }
-        
+
         return output;
-        
+
     }
-    
+
     public boolean httpLogin() {
 
         try {
             String output = httpGet("");
-            
+
             Document doc = Jsoup.parse(output);
             Elements forms = doc.select("form");
             String action = forms.attr("action");
-            
-            if (action.isEmpty()){
+
+            if (action.isEmpty()) {
                 return false;
             }
-                       
+
             Elements hidden = doc.select("input[type=hidden]");
-            List <NameValuePair> nvps = new ArrayList<>();
+            List<NameValuePair> nvps = new ArrayList<>();
             nvps.add(new BasicNameValuePair("email", USERNAME));
             nvps.add(new BasicNameValuePair("password", PASSWORd));
             nvps.add(new BasicNameValuePair("create", "0"));
-            
-            for (Element el1 : hidden){
-                 nvps.add(new BasicNameValuePair(el1.attr("name"), el1.attr("value")));
+
+            for (Element el1 : hidden) {
+                nvps.add(new BasicNameValuePair(el1.attr("name"), el1.attr("value")));
             }
-                        
+
             HttpPost httpPost = new HttpPost(action);
             httpPost.setHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13");
             httpPost.setHeader(HttpHeaders.REFERER, BASE_URL);
@@ -111,133 +116,92 @@ public class AmazonEchoApi {
             }
             System.out.println("Login successful");
             return true;
-            
+
         } catch (Exception e) {
             System.out.println("Login Error:" + e.getMessage());
             return false;
-        } 
-    }
-    
-    public boolean checkItemId(String itemId) throws IOException{
-        File file = new File("Items.txt");
-        boolean ret = false;
-        try {
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            Scanner scanner = new Scanner(file);
-            int lineNum = 0;
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                lineNum++;
-                if(line.contains(itemId)) { 
-                    ret = true;
-                }
-            }
-        } catch(Exception e) { 
-            ret = false;
         }
+    }
+
+    public boolean checkItemId(String itemId) {
+        boolean ret = false;
+        if (itemId.equalsIgnoreCase(LastText)) {
+            ret = true;
+        }
+
+        LastText = itemId;
         return ret;
     }
-    
-    public void addItemId(String itemId) throws IOException{
-        File file =new File("Items.txt");
-        try {
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            FileWriter fileWritter = new FileWriter(file.getName(),true);
-            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-            bufferWritter.write(itemId + "\n");
-            bufferWritter.close();
-        } catch(Exception e) { 
-        }
-    }
-    
-    public String getLatestTodo() throws IOException{
 
-      String output = httpGet("/api/todos?type=TASK&size=1");
-    //   System.out.println(output);
-     // Parse JSON
+    public String getLatestTodo() throws IOException {
+
+        String output = httpGet("/api/todos?type=TASK&size=1");
+        //   System.out.println(output);
+        // Parse JSON
         Object obj = JSONValue.parse(output);
         JSONObject jsonObject = (JSONObject) obj;
         JSONArray values = (JSONArray) jsonObject.get("values");
-        JSONObject item = (JSONObject)values.get(0);
+        JSONObject item = (JSONObject) values.get(0);
 //System.out.println(item);
         // Get text and itemId
         String text = item.get("text").toString();
         String itemId = item.get("itemId").toString();
 
-        if (!checkItemId(itemId)){
-            addItemId(itemId);
+        if (!checkItemId(itemId)) {
+
             return text;
-        }else{
+        } else {
             return null;
         }
     }
-   public String getLatestCards() throws IOException{
-      
-     String output = httpGet("/api/cards");
-      System.out.println(output);
-                // Parse JSON
+
+    public String getLatestCards() throws IOException {
+
+        String output = httpGet("/api/cards");
+        System.out.println(output);
+        // Parse JSON
         Object obj = JSONValue.parse(output);
         JSONObject jsonObject = (JSONObject) obj;
         JSONArray values = (JSONArray) jsonObject.get("cards");
 //System.out.println(values.size()); 
 
-JSONObject item = (JSONObject)values.get(0);
+        JSONObject item = (JSONObject) values.get(0);
 
 //System.out.println(item.toString());
-
         // Get text and itemId
         String text = item.get("title").toString();
-       String itemId = item.get("title").toString();
+        String itemId = item.get("title").toString();
 //System.out.println(text);
-      
-      if (!checkItemId(itemId)){
-        addItemId(itemId);
-       return text;
-      }else{
-       return null;
-       }
-    }  
-    public String getLatestHistory() throws IOException{
+
+        if (!checkItemId(itemId)) {
+
+            return text;
+        } else {
+            return null;
+        }
+    }
+
+    public String getLatestHistory() throws IOException {
         String output = httpGet("/api/activities?startTime=&size=1&offset=-1");
-//       System.out.println(output);
-                // Parse JSON
-      
+      //System.out.println(output);
+        
+
+        // Parse JSON
         Object obj = JSONValue.parse(output);
         JSONObject jsonObject = (JSONObject) obj;
         JSONArray values = (JSONArray) jsonObject.get("activities");
-        //System.out.println(values.size()); 
-JSONObject item = (JSONObject)values.get(0);
-
-System.out.println(item.toString());
-
-        // Get text and itemId
+        JSONObject item = (JSONObject) values.get(0);
         String text = item.get("description").toString();
-       System.out.println("text="+text);
 //Get the summary text
-  //      Object obj2 = JSONValue.parse(text);
-    //    JSONObject jsonObject2 = (JSONObject) obj2;
-      //  JSONArray values2 = (JSONArray) jsonObject.get("summary");
+        Object obj2 = JSONValue.parse(text);
+        JSONObject jsonObject2 = (JSONObject) obj2;
+        text = jsonObject2.get("summary").toString();
 
-      
-      
-            
+        if (!checkItemId(text)) {
 
-      
-     // JSONObject item2=(JSONObject) values2.get(0);
-     // text = item2.get("summary").toString();
-     // String itemId = item2.get("firstStreamId").toString();
-    //  System.out.println("text="+text);
-
-      
-  //    if (!checkItemId(itemId)){
-    //    addItemId(itemId);
-       return text;
-     // }else{
-      // return null;
-      // }
-    }  
+            return text;
+        } else {
+            return null;
+        }
+    }
 }
